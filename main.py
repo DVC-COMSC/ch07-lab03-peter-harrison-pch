e07c7013a5b0c18aa17b355a27eed3ce223c1bb3
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-26 00:35:45 +0000

    fix should work

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 # ******************************
 # Make your Code
 # ******************************
+numbers = []
+iterat = 5
+for i in range(iterat):
+    numbers.append(int(input("Integer: ")))
+
+averag = sum(numbers) / len(numbers)
+
+answer = []
+for i in range(iterat):
+    if numbers[i] > averag:
+        answer.append(numbers[i])
+
+print(*answer)
